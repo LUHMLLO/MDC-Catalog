8bb00368f1faaaa72bb98581094bd7a409dcf45e
Luis Melo <l.melo@mdcgroup.local>
2021-11-01 16:30:38 -0400

    now generating a hidden folder where all images will be saved

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mdc_catalog/theme.dart';
 import 'package:mdc_catalog/screens.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'dart:io';
 
 void main() {
   runApp(const MyApp());
@@ -18,18 +19,8 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     requestPermission();
+    createFolder(".mdcgroup"); //this will be a hidden folder
     super.initState();
-  }
-
-  void requestPermission() async {
-    var storageStatus = await Permission.storage.status;
-    if (!storageStatus.isGranted) {
-      await Permission.storage.request();
-    }
-    var externalStorage = await Permission.manageExternalStorage.status;
-    if (!externalStorage.isGranted) {
-      await Permission.manageExternalStorage.request();
-    }
   }
 
   @override
@@ -41,5 +32,33 @@ class _MyAppState extends State<MyApp> {
         themeMode: ThemeMode.light,
         title: 'MDC Catalog',
         home: ScreensScaffold());
+  }
+}
+
+//functions and more
+void requestPermission() async {
+  var storageStatus = await Permission.storage.status;
+  if (!storageStatus.isGranted) {
+    await Permission.storage.request();
+  }
+
+  var externalStorage = await Permission.manageExternalStorage.status;
+  if (!externalStorage.isGranted) {
+    await Permission.manageExternalStorage.request();
+  }
+}
+
+Future<String> createFolder(String cow) async {
+  final folderName = cow;
+  final path = Directory("storage/emulated/0/$folderName");
+  var status = await Permission.storage.status;
+  if (!status.isGranted) {
+    await Permission.storage.request();
+  }
+  if ((await path.exists())) {
+    return path.path;
+  } else {
+    path.create();
+    return path.path;
   }
 }
